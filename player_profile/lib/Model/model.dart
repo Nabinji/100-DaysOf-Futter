a0c5f5463d155fd09a9db58dfa7f6bf56610893d
Adventure <adventure@Adventures-MacBook-Pro-16.local>
2024-03-12 13:49:17 +0545

    player profiel

---
+++ player_profile/lib/Model/model.dart
@@ -0,0 +1,46 @@
+final List<dynamic> players = [
+  {
+    'name': 'Leo\nMessi',
+    'about':
+        'Lionel Andrés "Leo" Messi is an Argentine professional footballer who plays as a forward',
+    'gamePlay': '1045 games ',
+    'totalGoal': '821 goals',
+    'image':
+        'https://i.pinimg.com/736x/42/69/2d/42692d77e35e33e35c9750268c4b71f1.jpg'
+  },
+  {
+    'name': 'Cristiano\nRonaldo',
+    'gamePlay': '1200 games',
+    'totalGoal': '877 goals',
+    'about':
+        'Cristiano Ronaldo dos Santos Aveiro GOIH ComM is a Portuguese professional footballer who plays as a forward',
+    'image':
+        "https://qph.cf2.quoracdn.net/main-qimg-3e8f017c749da9d95f0a6b8f39d0de27-lq"
+  },
+  {
+    'name': 'Jude\nBellingham',
+    'gamePlay': '92 games',
+    'totalGoal': '12 goals',
+    'about':
+        'Jude Victor William Bellingham is an English professional footballer who plays as a midfielder',
+    'image':
+        'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEimPdqsgizFk1TY6f1VqYy2dVeWSG_XOhPUeFcg7zgJXpqgnFaFqW16BOO5FqJJPGaX0WGvuyo7I_GUNd7gGqt-dJqNiAp3-nJc6cAP1Nvx4cFpU5uhxWCPvSv0vtwAa_wCj4e9j9Jmc391ut38kajXffdyWwrhAmUXi7pVKoYRSlVPz6_7P4V71vJSalo/s1600/real-madrid-23-24-away-kit-1.jpg',
+  },
+  {
+    'name': 'Erling\nHaaland',
+    'gamePlay': '281 games',
+    'totalGoal': '234 goals',
+    'about':
+        'Erling Braut Haaland is a Norwegian professional footballer who plays as a striker',
+    'image':
+        "https://pbs.twimg.com/media/Fuq1xN8WAAEWlmu?format=jpg&name=4096x4096",
+  },
+  {
+    'name': 'Kylian\nMbappe',
+    'gamePlay': '407 games',
+    'totalGoal': '296 goals',
+    'about':
+        'Kylian Mbappé Lottin is a French professional footballer who plays as a forward',
+    'image': "https://www.enwallpaper.com/wp-content/uploads/2021/10/crop-8.jpg"
+  }
+];
---
+++ player_profile/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:player_profile/player_profile.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.sonoTextTheme(),
+      ),
+      home: const PlayerProfile(),
+    );
+  }
+}
