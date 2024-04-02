c7c25c8fcf0d857137deb3d94ea899303c85fce0
Adventure <adventure@Adventures-MacBook-Pro-16.local>
2024-04-02 12:48:11 +0545

    travel app

---
+++ travel_app/lib/Utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xffa3ecec);
+const secondaryColor = Color(0xff081053);
+const backgroundColor1 = Color(0xff041855);
+const backgroundColor2 = Color(0xff000002);
+const lightSecondary = Color(0xff1f2972);
